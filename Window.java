8c91e2688d3e42d9c5a0aed8f7d0501374299088
James Cline <james.cline@gatech.edu>
2011-12-20 14:04:01 -0500

    add "Add" button, does nothing atm

--- Window.java
+++ Window.java
@@ -12,16 +12,24 @@ public class Window
 	private JPanel topPanel,
 		buttons,
 		list;
+	private JButton add;
 
 	public Window()
 	{
+		bListener listener = new bListener();
+
 		window = new JFrame();
 
 		topPanel = new JPanel();
 		buttons = new JPanel();
 		list = new JPanel();
 
+		add = new JButton("Add");
+		add.addActionListener(listener);
+
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		buttons.add(add);
 
 		topPanel.add(list);
 		topPanel.add(buttons);
@@ -29,5 +37,14 @@ public class Window
 		window.add(topPanel);
 		window.pack();
 		window.setVisible(true);
+	}
+
+	private class bListener implements ActionListener
+	{
+		public void actionPerformed (ActionEvent e)
+		{
+			if(e.getSource() == add)
+				return;
+		}
 	}
 }
